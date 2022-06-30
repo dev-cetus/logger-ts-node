c436211dcaf32d3c7811e9212c5a76fa4ca3fb4f
dev-cetus <61233505+dev-cetus@users.noreply.github.com>
2022-06-30 19:59:35 +0200

    Support type Error

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,8 +1,7 @@
 export default class Logger {
-    #private;
-    static error(message: string, error?: boolean): void;
-    static info(message: string, error?: boolean): void;
-    static warn(message: string, error?: boolean): void;
-    static debug(message: string, error?: boolean): void;
-    static trace(message: string, error?: boolean): void;
+    static error(message: string | Error, error?: boolean): void;
+    static info(message: string | Error, error?: boolean): void;
+    static warn(message: string | Error, error?: boolean): void;
+    static debug(message: string | Error, error?: boolean): void;
+    static trace(message: string | Error, error?: boolean): void;
 }
